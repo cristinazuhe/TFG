30a8bd2b9e66be38f534217c045442b145e5992b
cristinazuhe <zuhe18@gmail.com>
2016-07-26 02:35:13 +0200

    representar cualquier braid

--- Matlab/prueba1.m
+++ Matlab/prueba1.m
@@ -27,66 +27,19 @@
 % axis equal;
 
 
-N_cortes=20;
-Radio=0.7;
-
-numero_braid1 = 1;
-numero_braid2 = 2;
-numero_braid3 = 3;
-numero_braid4 = 4;
-
-% contador=0;
-% cilindro_braid(numero_braid1,inicio,fin,N_cortes,Radio);
-% contador=contador+1;
-% hold on;
-% braid_neg(numero_braid2+contador,inicio,fin,N_cortes,Radio); %1--> de 1 a 3. 2 --> 2 a 4...
-% contador=contador+3;
-% cilindro_braid(numero_braid3+contador,inicio,fin,N_cortes,Radio);
-
 braid_cad_char = input('Ingrese braid (ej: +s2-s1+s1) ','s');
-contador_braid=1;
-numero_braid = zeros(1,length(braid_cad_char)/3);
+n_aux_cruces=1;
+indices_braid = zeros(1,length(braid_cad_char)/3);
 for i=1:length(braid_cad_char)
     if(strcmpi(braid_cad_char(i),'-'))
-       aux= braid_cad_char(i+2);
-       numero_braid(contador_braid) = -str2double(aux);
-       contador_braid = contador_braid+1;
+       indices_braid(n_aux_cruces) = -str2double(braid_cad_char(i+2));
+       n_aux_cruces = n_aux_cruces+1;
     elseif(strcmpi(braid_cad_char(i),'+'))
-       aux= braid_cad_char(i+2);
-       numero_braid(contador_braid) = str2double(aux);
-       contador_braid = contador_braid+1;
+       indices_braid(n_aux_cruces) = str2double(braid_cad_char(i+2));
+       n_aux_cruces = n_aux_cruces+1;
     end
 end
 
-cantidad_trenzas = max(abs(numero_braid))+1;
-contador=0;
-inicio = -3*pi;
-fin = 0;
-for i=1:length(numero_braid)
-    cruce = numero_braid(i);
-    
-    for j=1:abs(cruce)-1
-        cilindro_braid(j+contador,inicio,fin,N_cortes,Radio);
-        hold on;
-        contador = contador +1;
-    end
-    if(cruce < 0)
-        braid_neg(abs(cruce)+contador,inicio,fin,N_cortes,Radio); %1--> de 1 a 3. 2 --> 2 a 4...
-        hold on;
-    else   
-        braid_pos(abs(cruce)+contador,inicio,fin,N_cortes,Radio); %1--> de 1 a 3. 2 --> 2 a 4...
-        hold on;
-    end
-    contador = contador +3;
-    
-    for j=abs(cruce)+1:cantidad_trenzas-1
-        cilindro_braid(j+contador,inicio,fin,N_cortes,Radio);
-        hold on;
-        contador = contador +1;
-    end
-    
-    inicio = inicio -3*pi;
-    fin = fin -3*pi;
-    contador = 0; 
-end
-
+N_cortes=20;
+Radio=0.7;
+representar_braid(indices_braid, N_cortes, Radio);
