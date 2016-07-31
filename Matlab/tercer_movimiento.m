7959b2ab854a8c8190d777fcbc798bb279a7b0dc
cristinazuhe <zuhe18@gmail.com>
2016-07-31 20:31:27 +0200

    segundo movimiento (los 3 crossing)

--- Matlab/tercer_movimiento.m
+++ Matlab/tercer_movimiento.m
@@ -2,7 +2,7 @@ indices_braid=[3 -4 -2 ];
 
  for i=1:length(indices_braid)-1
     if(abs(abs(indices_braid(i)) - abs(indices_braid(i+1))) > 1)
-        nueva_braid = zeros(1,length(indices_braid)-2);
+        nueva_braid = zeros(1,length(indices_braid));
         for j=1:length(indices_braid)  %genero la nueva trenza sin los crossing i, i+1
             if(j==i)
                 nueva_braid(j) = indices_braid(j+1);
