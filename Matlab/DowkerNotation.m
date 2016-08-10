9d9142667706942ad8b56bc9d3fdcb98218854d7
cristinazuhe <zuhe18@gmail.com>
2016-08-10 02:11:10 +0200

    bien

--- Matlab/DowkerNotation.m
+++ Matlab/DowkerNotation.m
@@ -17,7 +17,7 @@ num_terms = length(indices_braid);
 vector_etiquetas = zeros(1,2*num_terms);
 
 string = 1; siguiente_etiqueta = 1; tipocross = 0; cross = 1; 
-string_encontrado = false; condicion = true; signo = 1;
+string_encontrado = false; condicion = true; signo = 1; link = false;
 
 while(condicion)
     string_encontrado = false;
@@ -39,37 +39,48 @@ while(condicion)
         end
     end
 
-    vector_etiquetas(2*(cross-1) + mod(siguiente_etiqueta,2)+1) = siguiente_etiqueta;
-    siguiente_etiqueta = siguiente_etiqueta +1;
-
-    if ((abs(indices_braid(cross)) == string && indices_braid(cross) * signo < 0) || ...
-        (abs(indices_braid(cross)) ~= string && indices_braid(cross) * signo > 0))
-            if (mod(siguiente_etiqueta,2))
-                vector_etiquetas(2*cross-1) = -vector_etiquetas(2*cross-1);
-            end 
-    end
-    
-    if ( abs(indices_braid(cross))== string)
-        string = string +1;
+    if (vector_etiquetas(2*(cross-1) + mod(siguiente_etiqueta,2)+1))
+        dowker = zeros(1,num_terms);
+        condicion = false;
+        link = true;
     else
-        string = string -1;
-    end
+        vector_etiquetas(2*(cross-1) + mod(siguiente_etiqueta,2)+1) = siguiente_etiqueta;
+        siguiente_etiqueta = siguiente_etiqueta +1;
 
-    cross = cross + 1;
-    condicion = (siguiente_etiqueta <= 2*num_terms);
+        if ((abs(indices_braid(cross)) == string && indices_braid(cross) * signo < 0) || ...
+            (abs(indices_braid(cross)) ~= string && indices_braid(cross) * signo > 0))
+                if (mod(siguiente_etiqueta,2))
+                    vector_etiquetas(2*cross-1) = -vector_etiquetas(2*cross-1);
+                end 
+        end
+
+        if ( abs(indices_braid(cross))== string)
+            string = string +1;
+        else
+            string = string -1;
+        end
+
+        cross = cross + 1;
+        condicion = (siguiente_etiqueta <= 2*num_terms);
+    end
 end
 
 %me quedo con los indices pares en el orden correcto. Sera la notación que
 %buscamos
-dowker = zeros(1,num_terms);
-for i=1:num_terms
-    for j=1:num_terms
-        if(vector_etiquetas(2*j) == (2*i-1))
-           dowker(i) = vector_etiquetas(2*j-1);
-           break;
+if (~link)
+    dowker = zeros(1,num_terms);
+    for i=1:num_terms
+        for j=1:num_terms
+            if(vector_etiquetas(2*j) == (2*i-1))
+               dowker(i) = vector_etiquetas(2*j-1);
+               break;
+            end
         end
     end
+    salida = strcat('Su Dowker Notation es: ',num2str(dowker));
+    disp(salida);
+else
+    disp('Se trata de link');
 end
 
-salida = strcat('Su Dowker Notation es: ',num2str(dowker));
-disp(salida);
+
