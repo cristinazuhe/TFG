eb9cfa0d12520516b6a5d07d781a5c8cfe98f8fe
cristinazuhe <zuhe18@gmail.com>
2016-09-11 01:50:32 +0200

    32

--- Matlab/primer_movimiento2.m
+++ Matlab/primer_movimiento2.m
@@ -1,9 +1,8 @@
-indices_braid=[-1 +1 ];
-
+indices_braid=[-3 +3 +2];
  for i=1:length(indices_braid)-1
     if(indices_braid(i) + indices_braid(i+1) == 0)
-        nueva_braid = zeros(1,length(indices_braid)-2);
         contador=1;
+        nueva_braid= [];
         for j=1:length(indices_braid)  %genero la nueva trenza sin los crossing i, i+1
             if(j~=i && j~=i+1)
             nueva_braid(contador) = indices_braid(j);
