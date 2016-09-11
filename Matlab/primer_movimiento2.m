d8e6ee6a2bf9ac30c71ed61b2d096331c440a0ee
cristinazuhe <zuhe18@gmail.com>
2016-09-11 18:07:59 +0200

    36

--- Matlab/primer_movimiento2.m
+++ Matlab/primer_movimiento2.m
@@ -1,22 +1,26 @@
-indices_braid=[-3 +3];
+indices_braid=[-1 +1 -3 +3];
+encontrado = false;
  for i=1:length(indices_braid)-1
-    if(indices_braid(i) + indices_braid(i+1) == 0)
-        contador=1;
-        nueva_braid = [];
-        braid_aux = indices_braid;
-        for j=1:length(indices_braid)  %genero la nueva trenza sin los crossing i, i+1
-            if(j~=i && j~=i+1)
-                nueva_braid(contador) = indices_braid(j);
-                contador= contador+1;
-            else
-                braid_aux(j)=0;    
+     if(~encontrado)
+        if(indices_braid(i) + indices_braid(i+1) == 0)
+            encontrado = true;
+            contador=1;
+            nueva_braid = [];
+            braid_aux = indices_braid;
+            for j=1:length(indices_braid)  %genero la nueva trenza sin los crossing i, i+1
+                if(j~=i && j~=i+1)
+                    nueva_braid(contador) = indices_braid(j);
+                    contador= contador+1;
+                else
+                    braid_aux(j)=0;    
+                end
+
             end
-                       
+            transicion_braids(indices_braid,braid_aux, 20,0.5);
+            if(max(abs(braid_aux)) ~= 0)
+                hold off;
+                transicion_braids(braid_aux,nueva_braid, 20,0.5);
+            end
         end
-        transicion_braids(indices_braid,braid_aux, 20,0.5);
-        if(max(abs(braid_aux)) ~= 0)
-            hold off;
-            transicion_braids(braid_aux,nueva_braid, 20,0.5);
-        end
-    end
+     end
 end
--- Matlab/segundo_movimiento2.m
+++ Matlab/segundo_movimiento2.m
@@ -1,21 +1,25 @@
-indices_braid=[-3 -2 +1 +2];
+indices_braid=[-3 -2 +1 +2 +2];
+encontrado = false;
 
  for i=1:length(indices_braid)-2
-    if((indices_braid(i)*indices_braid(i+1) > 0) || (indices_braid(i+1)*indices_braid(i+2) > 0))%necesito que los signos sean iguales.
-        if(abs(indices_braid(i)) == abs(indices_braid(i+2)))
-            nueva_braid = zeros(1,length(indices_braid));
-            for j=1:length(indices_braid)  
-                if(j==i)
-                    nueva_braid(j) = sign(indices_braid(j+2))*abs(indices_braid(j+1));
-                    nueva_braid(j+1) = sign(indices_braid(j+1))*abs(indices_braid(j));
-                    nueva_braid(j+2) = sign(indices_braid(j))*abs(indices_braid(j+1));
-                elseif (j~=(i+1) && j~=(i+2))
-                    nueva_braid(j) = indices_braid(j);
+     if(~encontrado)
+        if((indices_braid(i)*indices_braid(i+1) > 0) || (indices_braid(i+1)*indices_braid(i+2) > 0))%necesito que los signos sean iguales.
+            if(abs(indices_braid(i)) == abs(indices_braid(i+2)))
+                encontrado = true;
+                nueva_braid = zeros(1,length(indices_braid));
+                for j=1:length(indices_braid)  
+                    if(j==i)
+                        nueva_braid(j) = sign(indices_braid(j+2))*abs(indices_braid(j+1));
+                        nueva_braid(j+1) = sign(indices_braid(j+1))*abs(indices_braid(j));
+                        nueva_braid(j+2) = sign(indices_braid(j))*abs(indices_braid(j+1));
+                    elseif (j~=(i+1) && j~=(i+2))
+                        nueva_braid(j) = indices_braid(j);
+                    end
+
                 end
+                transicion_braids(indices_braid,nueva_braid, 20,0.5);
 
             end
-            transicion_braids(indices_braid,nueva_braid, 20,0.5);
-
         end
-    end
+     end
 end
